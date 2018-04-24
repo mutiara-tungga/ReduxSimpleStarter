a63f9082e9c4f1070a125832fdbfe19bac92043c
Putri Mutiara Tungga <putri@skyshi.com>
2018-04-24 09:32:37 +0700

    04 Managing App State with Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title : 'A', pages: 101 },
+    { title : 'B', pages: 102 },
+    { title : 'C', pages: 104 },
+    { title : 'D', pages: 100 } 
+  ]
+}
